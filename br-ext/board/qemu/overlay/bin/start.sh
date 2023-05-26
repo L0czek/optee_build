8750a0dd827b84b0cb62dce2988980186dc201c7
Michał Szaknis <michalszak@gmail.com>
2023-05-26 23:11:10 +0200

    Add additional fuzzing modes

--- br-ext/board/qemu/overlay/bin/start.sh
+++ br-ext/board/qemu/overlay/bin/start.sh
@@ -3,6 +3,10 @@ set -exou pipefail
 for arg in $(cat /proc/cmdline); do
     if [ "$arg" = 'fuzz' ]; then
         fuzzer-rs;
+    elif [ "$arg" = 'host_fuzz' ]; then
+        fuzzer-rs --host_fuzzer
+    elif [ "$arg" = "fuzz_no_reverts" ]; then
+        fuzzer-rs -n
     else
         OPT=$(echo "$arg" | cut -d '=' -f 1);
         if [ "$OPT" = "testcase" ]; then
