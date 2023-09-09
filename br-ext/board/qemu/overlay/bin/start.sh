f312c8bb6dda35ed92c20815b6d5057856c59def
Michał Szaknis <michalszak@gmail.com>
2023-09-09 14:48:17 +0200

    Add test case decoding mode switch

--- br-ext/board/qemu/overlay/bin/start.sh
+++ br-ext/board/qemu/overlay/bin/start.sh
@@ -1,12 +1,14 @@
 set -exou pipefail
 
+MODE=dsl
+
 for arg in $(cat /proc/cmdline); do
     if [ "$arg" = 'fuzz' ]; then
-        fuzzer-rs;
+        fuzzer-rs -m$MODE;
     elif [ "$arg" = 'host_fuzz' ]; then
-        fuzzer-rs --host_fuzzer
+        fuzzer-rs --host_fuzzer -m$MODE
     elif [ "$arg" = "fuzz_no_reverts" ]; then
-        fuzzer-rs -n
+        fuzzer-rs -n -m$MODE
     elif [ "$arg" = "tcgen" ]; then
         fuzzer-rs -g
     elif [ "$arg" = "exit" ]; then
@@ -14,7 +16,9 @@ for arg in $(cat /proc/cmdline); do
     else
         OPT=$(echo "$arg" | cut -d '=' -f 1);
         if [ "$OPT" = "testcase" ]; then
-            fuzzer-rs -t $(echo "$arg" | cut -d '=' -f 2);
+            fuzzer-rs -t $(echo "$arg" | cut -d '=' -f 2) -m$MODE
+        elif [ "$OPT" = "mode" ]; then
+            MODE=$(echo "$arg" | cut -d '=' -f 2)
         fi
     fi
 done
